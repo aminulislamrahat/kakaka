c711ddb0062ee5b0dca6c03babb3dace314abddf
Ghost_Rahat <62211936+aminulislamrahat@users.noreply.github.com>
2020-09-08 21:07:07 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Local Css  -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="resources/css/main.css">
+	<title>FreeCookers</title>
+</head>
+<body>
+	<header>
+		<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+			<div class="container">
+				<a class="navbar-brand">FreeCookers</a>
+				<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div id="navbarNav" class="collapse navbar-collapse">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item"><a href="#" class="nav-link">HOME</a></li>
+					<li class="nav-item"><a href="#" class="nav-link">ABOUT</a></li>
+					<li class="nav-item"><a href="#" class="nav-link">SERVICES</a></li>
+					<li class="nav-item"><a href="#" class="nav-link">CONTACT</a></li>
+				</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+
+	<div class="container">
+		<section id = "services">
+		<div>
+			<h2>services</h2>
+			<p>we have some few services
+				<ul>they are
+					<li>1</li>
+					<li>2</li>
+					<li>3</li>
+					<li>4</li>
+				</ul>
+			</p>
+			
+		</div>
+	
+	</section>
+
+	<section id = "address">
+		<div>
+			<h2>address</h2>
+			<p>we have some shops
+				<ul>they are
+					<li>1</li>
+					<li>2</li>
+					<li>3</li>
+				</ul>
+			</p>
+		</div>
+	</section>
+
+	<footer class="bg-dark text-white text-center">
+<div id="about_us">
+			         <h2>About us</h2>
+			         <p>kichu kotha about dokan</p>
+		        </div>
+
+		<div class="row">
+			<div class="col-md-4">
+			<div>
+				<h2>Contact Number</h2>
+				<ul>
+					<li>number1</li>
+					<li>number2</li>
+					<li>number3</li>
+				</ul>
+			</div>
+				
+			</div>
+			<div class="col-md-4">
+				<h2>Follow us</h2>
+				<ul>
+					<li><a href="#"> Facebook logo </a></li>
+					<li><a href="#"> twitter logo </a></li>
+					<li><a href="#"> g-mail logo </a></li>
+					<li><a href="#"> foodpanda logo </a></li>
+				</ul>
+				
+			</div>
+			<div class="col-md-4">
+				<h2>Contact us</h2>
+				<label for="name">Name : </label><br>
+				<input type="text" name="name" id="name"><br><br>
+				<label for="E-mail">E-mail : </label><br>
+				<input type="email"  name="E-mail" id="E-mail"><br><br>
+				<label for="massage"> Write messege : </label>
+				<textarea name="messege" id="messege" cols="30" rows="10"></textarea><br>
+				<input type="button" value="SEND">
+				
+			</div>
+		</div>
+		
+		<div id="cont_shop">
+			<div >
+			</div>
+			<div >
+			</div>
+		</div>
+		<div id="copy_right">
+			<p>copyright massage </p>
+			
+		</div>
+	</footer>
+	</div>
+	<!-- Jquery -->
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous">
+	</script>
+	<!-- bootstrap js -->
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
